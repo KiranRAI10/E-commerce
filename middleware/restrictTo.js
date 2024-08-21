c7bc58c01795f32220462ef73d6a3332a20870a7
Kiran Rai <raikira77@gmail.com>
2024-08-22 00:02:36 +0545

    slight error in database key name

--- middleware/restrictTo.js
+++ middleware/restrictTo.js
@@ -1,8 +1,9 @@
 
-const restrictTo = (...roles) =>{
+const restrictTo = (...role) =>{
     return (req,res,next) =>{
-        const userRole = req.user.roles
-        if(!roles.includes(userRole)){
+        const userRole = req.user.role
+
+        if(!role.includes(userRole)){
             res.status(403).json({
                 message : "You don't have permission to perform this action"
             })
