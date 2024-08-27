8d641a61d4d7fa0a99c395975f9f690d73244ec2
Kiran Rai <raikira77@gmail.com>
2024-08-27 13:10:51 +0545

    delete user by admin

--- controller/admin/user/userController.js
+++ controller/admin/user/userController.js
@@ -2,7 +2,8 @@ const User = require("../../../model/userModel")
 
 
 exports.getUsers = async (req,res) =>{
-    const users = await User.find().select("-__v")
+    const userId = req.user.id
+    const users = await User.find({_id : {$ne : userId}}).select("-__v")
     if(users.length > 1){
         res.status(200).json({
             message : "Users fetched successfully",
@@ -14,4 +15,27 @@ exports.getUsers = async (req,res) =>{
             data : []
         })
     }
+}
+
+//delete user API
+exports.deleteUser = async (req,res) =>{
+    const userId = req.params.id
+    if(!userId){
+        return res.status(400).json({
+            message : "Please provide user id"
+        })
+    }
+    //check if that user exists or not
+    const user = await User.findById(userId)
+    if(!user){
+        res.status(404).json({
+            message : "User not found with that user id"
+        })
+    }else {
+        
+        await User.findByIdAndDelete(userId)
+        res.status(200).json({
+            message : "User removed successfully"
+    })
+}
 }
